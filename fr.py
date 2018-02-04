3bd2547630bb78a62b9f7ed0f1b23811ba600000
Mograbi <mograbi.moawiya@gmail.com>
2018-02-04 15:12:10 +0000

    removed file pointer from class, added gitignore

--- fr.py
+++ fr.py
@@ -2,7 +2,6 @@
 
 class FileReader(object):
 	__path = None
-	__file = None
 	__content_lines = None
 	__cur_line = -1
 	__EOF = False
@@ -15,13 +14,16 @@ class FileReader(object):
 	def read(self):
 		if self.__path is None:
 			raise Exception("file path is missing")
-		self.__file = open(self.__path, 'r')
-		self.__content_lines = self.__file.readlines()
+		f = open(self.__path, 'r')
+		self.__content_lines = f.readlines()
+		f.seek(0)
+		self.__content = f.read()
+		f.close()
 		self.__iter = iter(self.__content_lines)
-		self.__cur_line = 
+		self.__cur_line = 0
 
 	def next_line(self):
-		if self.__file is None:
+		if self.__content_lines is None:
 			raise Exception("file not initialized !!")
 		try:
 			line = next(self.__iter)
@@ -37,16 +39,11 @@ class FileReader(object):
 	def get_line(self, line_num):
 		if line_num < 0 or line_num > len(self.__content_lines):
 			raise Exception("get_line: out of bound ERROR !!")
-		if self.__file is None:
-			raise Exception("get_line ERROR")
 		return self.__content_lines[line_num - 1]
 
 	def close(self):
-		if self.__file is not None:
-			self.__file.close()
-			self.__file = None
-			self.__cur_line = -1
-			self.__iter = None
+		self.__cur_line = -1
+		self.__iter = None
 
 	def EOF(self):
 		return self.__EOF == True
@@ -61,7 +58,6 @@ fr = FileReader("D:\\tmp.txt")
 line = fr.next_line()
 while not fr.EOF():
 	# TODO: write your code here
-
 	line = fr.next_line()
 
 fr.close()
